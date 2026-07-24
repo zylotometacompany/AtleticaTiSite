23e470824e25e8e8c23eee7d0d87b9698e4fcd84
Nicolas Enoque <nicolasenoque0@gmail.com>
2026-07-23 23:27:56 -0300

    fix: URL api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3333",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 /* api.interceptors.request.use(
