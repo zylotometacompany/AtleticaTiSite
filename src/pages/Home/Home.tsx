880f2ab1e6c9592739c0a0129aaaae3cbf5685d8
Zyloto Meta <nicolas.enoque@icloud.com>
2026-08-16 21:59:49 -0300

    fiz background fotos

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -27,7 +27,7 @@ import smokeLogo from "../../assets/smoke-removebg-preview.png";
 import coposLogo from "../../assets/copos-removebg-preview.png";
 import kairon from "../../assets/kairon-removed.png";
 import pablo from "../../assets/pablo-removebg-preview.png";
-import araujo from "../../assets/WhatsApp Image 2026-08-16 at 21.09.22.jpeg"
+import araujo from "../../assets/ana.png"
 import "./Home.css";
 import "./Home.carousel.css";
 import "./Home.sections.css";
