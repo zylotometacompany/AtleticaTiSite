a892c9ef1f7982208aa4d1bf2cba200440d3372f
Zyloto Meta <nicolas.enoque@icloud.com>
2026-08-21 15:10:59 -0300

    disable loja

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,9 @@ import { MainLayout } from "./layout/MainLayout";
 import Home from "./pages/Home/Home";
 import Events from "./pages/Events/Events";
 import Diretoria from "./pages/Directors/Diretoria";
-import ZCardRequest from "./pages/zCardRequest/zCardRequest";
-
-import { ProductsPage } from "./pages/store/Store";
 
 import { CheckoutPage } from "./pages/store/Checkout/Checkout";
-import PedidoSucessoPage from "./pages/store/PedidoSucessoPage/PedidoSucessoPage";
-import PedidoPendentePage from "./pages/store/PedidoPendentePage/PedidoPendentePage";
-import PedidoFalhaPage from "./pages/store/PedidoFalhaPage/PedidoFalhaPage";
-import PedidoComprovantePage from "./pages/store/PedidoComprovantePage/PedidoComprovantePage";
+
 
 export default function App() {
   return (
@@ -25,31 +19,10 @@ export default function App() {
 
         <Route path="/eventos" element={<Events />} />
 
-        <Route path="/zcard" element={<ZCardRequest />} />
-
-        <Route path="/loja" element={<ProductsPage />} />
+      
 
         <Route path="/checkout" element={<CheckoutPage />} />
 
-        <Route
-          path="/pedido/:publicToken/sucesso"
-          element={<PedidoSucessoPage />}
-        />
-
-        <Route
-          path="/pedido/:publicToken/pendente"
-          element={<PedidoPendentePage />}
-        />
-
-        <Route
-          path="/pedido/:publicToken/falha"
-          element={<PedidoFalhaPage />}
-        />
-
-        <Route
-          path="/pedido/:publicToken/comprovante"
-          element={<PedidoComprovantePage />}
-        />
       </Route>
     </Routes>
   );
